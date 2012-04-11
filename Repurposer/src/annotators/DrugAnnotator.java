6224dee8995fc5774210bcfbc8892948e194da7b
Samuel Croset <samuel.croset@gmail.com>
2012-04-11 11:20:58 +0100

    Connection with dictionaries done

--- Repurposer/src/annotators/DrugAnnotator.java
+++ Repurposer/src/annotators/DrugAnnotator.java
@@ -3,18 +3,20 @@
  */
 package annotators;
 
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.AnalysisComponent;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.ResourceAccessException;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import resources.Dictionary;
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.Chunking;
+import com.aliasi.dict.ExactDictionaryChunker;
+import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+
+import dictionaries.Dictionary;
+import dictionaries.DrugBankDictionary;
 
 import bioentities.Drug;
 
@@ -24,57 +26,46 @@ import bioentities.Drug;
  */
 public class DrugAnnotator extends JCasAnnotator_ImplBase{
     /** Map from acronyms to their expanded forms */
-    private Dictionary mMap;
+    private DrugBankDictionary dico;
 
     /**
      * @see AnalysisComponent#initialize(UimaContext)
      */
     public void initialize(UimaContext aContext) throws ResourceInitializationException {
-      super.initialize(aContext);
-      // get a reference to the String Map Resource
-      try {
-        mMap = (Dictionary) getContext().getResourceObject("DrugDictionary");
-      } catch (ResourceAccessException e) {
-        throw new ResourceInitializationException(e);
-      }
+	super.initialize(aContext);
+
+	String path = (String) aContext.getConfigParameterValue("DrugDictionaryPath");
+	dico = new DrugBankDictionary();
+	System.out.println("new dico created");
+	System.out.println("starts loading...");
+	dico.load(path);
+	System.out.println("loading done");
     }
 
     /**
      * @see JCasAnnotator_ImplBase#process(JCas)
      */
     public void process(JCas aJCas) {
-      // go through document word-by-word
-      String text = aJCas.getDocumentText();
-      
-     
-      Pattern mYorktownPattern = Pattern.compile(mMap.get("STM"));
-      Matcher matcher = mYorktownPattern.matcher(text);
-      while (matcher.find()) {
-        // found one - create annotation
-        Drug annotation = new Drug(aJCas);
-        annotation.setBegin(matcher.start());
-        annotation.setEnd(matcher.end());
-        annotation.setName("Yorktown");
-        annotation.addToIndexes();
-      }
 
-      
-      
-      int pos = 0;
-      StringTokenizer tokenizer = new StringTokenizer(text, " \t\n\r.<.>/?\";:[{]}\\|=+()!", true);
-      while (tokenizer.hasMoreTokens()) {
-        String token = tokenizer.nextToken();
-        // look up token in map to see if it is an acronym
-        String expandedForm = mMap.get(token);
-        if (expandedForm != null) {
-          // create annotation
-          Drug annot = new Drug(aJCas);
-          annot.setName(expandedForm);
-          annot.addToIndexes();
-        }
-        // incrememnt pos and go to next token
-        pos += token.length();
-      }
+	String text = aJCas.getDocumentText();
+	System.out.println("creating lingpipe dico");
+	ExactDictionaryChunker chunker = new ExactDictionaryChunker(dico.getLingPipeDictionary(), IndoEuropeanTokenizerFactory.INSTANCE, true,false);
+
+	
+	Chunking chunking = chunker.chunk(text);
+	System.out.println("iterates over the chunks: " + chunking.chunkSet().size());
+	for (Chunk chunk : chunking.chunkSet()) {
+	    int start = chunk.start();
+	    int end = chunk.end();
+	    String type = chunk.type();
+	    
+	    Drug annotation = new Drug(aJCas);
+	    annotation.setBegin(start);
+	    annotation.setEnd(end);
+	    annotation.setName(type);
+	    annotation.addToIndexes();
+	    
+	}
     }
 
 }
