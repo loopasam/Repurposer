916db6e9e5742deb00654db021263785c18df3dc
Samuel Croset <samuel.croset@gmail.com>
2012-04-16 10:13:41 +0100

    multi-thread working

--- Repurposer/src/ClientTest.java
+++ Repurposer/src/ClientTest.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.uima.UIMAFramework;
@@ -31,13 +32,20 @@ import bioentities.Drug;
  *
  */
 public class ClientTest {
+    
+    public static int numberOfCas = 0;
+    public static Date dateStart;
 
     private static class CallbackListener extends UimaAsBaseCallbackListener {
 	@Override
 	public void entityProcessComplete(CAS cas, EntityProcessStatus aStatus) {
+	    
+	    
+	
 	    JCas jcas = null;
 	    try {
 		jcas = cas.getJCas();
+		numberOfCas++;
 	    } catch (CASException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -69,7 +77,7 @@ public class ClientTest {
 	HashMap<String, Object> context = new HashMap<String, Object>();
 	context.put(UimaAsynchronousEngine.ServerUri, "tcp://samuel-Latitude-E6510:61616");
 	context.put(UimaAsynchronousEngine.Endpoint, "sam_test");
-	context.put(UimaAsynchronousEngine.CasPoolSize, 1);
+	context.put(UimaAsynchronousEngine.CasPoolSize, 4);
 	context.put(UimaAsynchronousEngine.SerializationStrategy, "binary");
 
 	CollectionReaderDescription collectionReaderDescription = UIMAFramework.getXMLParser()
@@ -79,8 +87,20 @@ public class ClientTest {
 
 	asClient.setCollectionReader(collectionReader);
 	asClient.initialize(context);
+	dateStart = new Date();
+	System.out.println("time starts: " + dateStart);
+
 	asClient.process();
 	asClient.collectionProcessingComplete();
+	System.out.println("Stopping");
+	System.out.println("number of cas processed: " + numberOfCas);
+	
+	Date currentDate = new Date();
+	
+	int secondsStart = dateStart.getHours()*60*60 + dateStart.getMinutes()*60 + dateStart.getSeconds();
+	int secondsEnd = currentDate.getHours()*60*60 + currentDate.getMinutes()*60 + currentDate.getSeconds();
+	
+	System.out.println("time: " + (secondsEnd - secondsStart) + " seconds");
 	asClient.stop();
 
     }
